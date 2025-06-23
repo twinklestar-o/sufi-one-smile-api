31c36408e9331a4579d700949154c70e137fcd30
Yessi Sitanggang <149045250+ecii123@users.noreply.github.com>
2025-06-23 10:54:48 +0700

    update db direct visit

--- app/Models/DirectVisit.php
+++ app/Models/DirectVisit.php
@@ -1,12 +1,22 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class DirectVisit extends Model {
-protected $fillable = [
-'jabatan_saya','area_code','branch_code','product_code','dealer_code',
-'dari_tanggal','sampai_tanggal','tanggal_selesai',
-'nama_pic','theme_of_discussion','problem','follow_up','description',
-'photo1_path','photo2_path','latitude','longitude'
-];
+
+class DirectVisit extends Model
+{
+    protected $fillable = [
+      'jabatan_saya','area_code','branch_code','product_code','dealer_code',
+      'tipe_visit','tujuan_visit',
+      'dari_tanggal','sampai_tanggal','tanggal_selesai',
+      'nama_pic','theme_of_discussion','problem','follow_up','description',
+      'photo1_path','photo2_path','latitude','longitude'
+    ];
+
+   public function mainPersons()
+{
+    return $this->hasMany(DirectVisitMainPerson::class, 'direct_visit_id');
+}
+
 }
