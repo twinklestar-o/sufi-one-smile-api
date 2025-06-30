5991df3eafa9f71d618f567bb95378a224908148
Reinaldy Hutapea <reinaldyhutapea@gmail.com>
2025-06-30 16:45:03 +0700

    commenting areaseeder (duplicated)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AreaSeeder::class,
+            // AreaSeeder::class,
             CollectionsSeeder::class,
             JabatanSeeder::class,
             JabatanSFISeeder::class,
